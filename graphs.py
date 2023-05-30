6ac2c80549dcc39de0c7f7a12012231395c37a1f
shalazary <dfcz.petro@gmail.com>
2023-05-30 19:03:23 +0300

    Add extra comment

--- graphs.py
+++ graphs.py
@@ -19,6 +19,7 @@ def dfs(graph, start, visited=None, to=0, path_len=0):
     for next in graph[start] - visited:
         path_len += 1
         _, _, is_find = dfs(graph, next, visited, to=to, path_len=path_len)
+        # Very very code
         if is_find:
             return visited.union({next}), path_len + 1, True
 
