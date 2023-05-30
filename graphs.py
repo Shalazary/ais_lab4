b0930c0e00a0b4e4b767efa73b96748883dd1d86
shalazary <dfcz.petro@gmail.com>
2023-05-30 18:26:00 +0300

    Add path finding

--- graphs.py
+++ graphs.py
@@ -1,10 +1,16 @@
-def dfs(graph, start, visited=None):
+def dfs(graph, start, visited=None, to=0, path_len=0):
+    if start == to:
+        return visited, path_len, True
+
     if visited is None:
         visited = set()
+
     visited.add(start)
 
-    print(start)
-
     for next in graph[start] - visited:
-        dfs(graph, next, visited)
-    return visited
+        path_len += 1
+        _, _, is_find = dfs(graph, next, visited, to=to, path_len=path_len)
+        if is_find:
+            return visited.union({next}), path_len + 1, True
+
+    return visited, path_len, False
